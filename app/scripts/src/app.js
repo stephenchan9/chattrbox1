a95cca6b10bd5d0b89e7e2bcef18086ab6a08d8f
Stephen <stephen.chan9@yahoo.com>
2017-04-19 21:16:12 -0700

    changes made

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,6 +1,6 @@
-class ChatApp{
-  constructor(){
-    console.log('Hello Es6!');
-  }
+class ChatApp {
+    constructor() {
+        console.log('Hello Es6!');
+    }
 }
 export default ChatApp;
--- websockets-server.js
+++ websockets-server.js
@@ -2,24 +2,24 @@ var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server;
 var port = 3001;
 var ws = new WebSocketServer({
-  port: port
+    port: port
 });
-var messages =[];
+var messages = [];
 
 console.log('websockets server started');
 
-ws.on('connection', function(socket){
-  console.log('client connection established');
+ws.on('connection', function(socket) {
+    console.log('client connection established');
 
-  messages.forEach(function(msg){
-    socket.send(msg);
-  });
-
-  socket.on('message', function(data){
-    console.log('message recieved: '+data);
-    messages.push(data);
-    ws.clients.forEach(function (clientSocket){
-      clientSocket.send(data);
+    messages.forEach(function(msg) {
+        socket.send(msg);
     });
-  });
+
+    socket.on('message', function(data) {
+        console.log('message recieved: ' + data);
+        messages.push(data);
+        ws.clients.forEach(function(clientSocket) {
+            clientSocket.send(data);
+        });
+    });
 });
